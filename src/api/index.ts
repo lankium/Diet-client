98ec71cd42dbb7979a7a77c216857afa8339a6a6
廖建辉 <lankium@gmail.com>
2024-10-24 17:56:31 +0800

    axios.baseURL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,27 +1,26 @@
 import axios from "axios";
-import { showToast } from 'vant';
+import { showToast } from "vant";
 
-axios.defaults.baseURL = 'http://8.149.240.91:3000';
-axios.defaults.headers.post['Content-Type'] = 'application/json' // 响应
+axios.defaults.baseURL = "http://104.243.28.115:3000";
+axios.defaults.headers.post["Content-Type"] = "application/json"; // 响应
 
 // 响应拦截
-axios.interceptors.response.use(res => {
-  if (res.status !== 200) { // 程序错误
-    showToast('服务器异常');
-    return Promise.reject(res)
+axios.interceptors.response.use((res) => {
+  if (res.status !== 200) {
+    // 程序错误
+    showToast("服务器异常");
+    return Promise.reject(res);
   } else {
-    return res
+    return res;
   }
-})
+});
 
-export default axios
+export default axios;
 // 请求拦截
-axios.interceptors.request.use(req => {
-  let jwtToken = sessionStorage.getItem('token')
+axios.interceptors.request.use((req) => {
+  let jwtToken = sessionStorage.getItem("token");
   if (jwtToken) {
-    req.headers.Authorization = jwtToken
+    req.headers.Authorization = jwtToken;
   }
-  return req
-})
-
-
+  return req;
+});
